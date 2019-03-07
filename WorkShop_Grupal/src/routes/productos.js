d8e112616a616887c89fa340b4e292490bf6acb4
Kendall Rentana <kendallandreyr@gmail.com>
2019-03-07 14:50:03 -0600

    Se crearon los controles para las categorias ( insertar, leer, actulizar, borrar ).

--- WorkShop_Grupal/src/routes/productos.js
+++ WorkShop_Grupal/src/routes/productos.js
@@ -106,9 +106,88 @@ router.post('/producto/', (req, res) => {
           console.log(err);
         }
       })
-  })
+  });
 
 
 /* Esta parte es controlar todo lo que tiene que ver con categorias */
+
+// extrae todo los productos de la tabla
+router.get('/categoria/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM categorias;', (err, rows, fields) => {
+      if(!err) {
+        res.json(rows);
+
+        // Devolvemos una respuesta en JSON
+        res.status(200).send({
+            menssage: 'ok'
+        });
+      } else {
+        console.log(err);
+      }
+    });  
+});
+
+//este es para insertar nuevos datos en la base de datos
+router.post('/categoria/', (req, res) => {
+    const {nombre_categoria, descripcion_categoria} = req.body;
+     query = 'INSERT INTO categorias(nombre_categoria, descripcion_categoria) VALUES (?,?)';
+    
+    if(nombre_categoria != null && nombre_categoria != ""){
+        mysqlConnection.query(query, [nombre_categoria, descripcion_categoria], (err, rows, fields) => {
+        if(!err){
+            // Devolvemos una respuesta en JSON
+            res.status(201).send({
+                menssage: 'Created'
+            });
+        }else{
+            console.log(err); 
+        }
+        }); 
+    }
+  });
+  
+  //esta funcion es para acutulizar los datos en la base de datos
+  router.put('/categoria/:id_categoria', (req, res) => {
+    const {nombre_categoria, descripcion_categoria} = req.body;
+    const {id_categoria} = req.params;
+    const query = `UPDATE categorias SET nombre_categoria=?,descripcion_categoria=? WHERE id_categoria=?`;
+    
+    if(nombre_categoria != null && nombre_categoria != ""){
+      if(descripcion_categoria != null && descripcion_categoria != ""){
+        mysqlConnection.query(query, [nombre_categoria, descripcion_categoria, id_categoria], (err, rows, fields) => {
+            if(!err){
+                res.status(200).send({
+                    menssage: 'actualizo correctamente.'
+                });
+            }else{
+                console.log(err);
+            }
+            });
+      }else{
+        res.status(202).send({
+            menssage: 'Falta descripcion.'
+        });
+      } 
+    }else{
+        res.status(202).send({
+            menssage: 'Falta categoria.'
+        });
+    } 
+  });
+
+  //esta fucion es para eliminar una categoria
+  router.delete('/categoria/:id_categoria', (req, res) => {
+    const {id_categoria} = req.params;
+    mysqlConnection.query('DELETE FROM categorias WHERE id_categoria = ?', [id_categoria] , (err, rows,
+      fields) => {
+        if(!err){
+            res.status(204).send({
+                menssage: 'No Content'
+            });
+        }else{
+          console.log(err);
+        }
+      })
+  })
 
 module.exports = router;
