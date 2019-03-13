34b6f8f1336a90ba3fa654945e15f7055db6e530
Kendall Rentana <kendallandreyr@gmail.com>
2019-03-12 23:48:25 -0600

    se agrego la sesiones para validar que usuario hace uso de la api

--- WorkShop_Grupal/src/index.js
+++ WorkShop_Grupal/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var session = require('express-session');
 
 
 // Settings
@@ -8,8 +9,43 @@ app.set('port', process.env.PORT || 3000);
 // Middlewares
 app.use(express.json());
 
+
+app.use(session({secret: 'kendall'}));
+
+var sess;
+var pass;
+
+app.post('/login', (req, res, next) => {
+  req.session.user = req.body.user;
+  req.session.password = req.body.password;
+
+  if(req.session.user == "kendall" && req.session.password == "2812"){
+    sess = req.session.user;
+    pass = req.session.password;
+    console.log(sess);
+    res.end('Sesion abierta.');
+  }else{
+    res.status(401).send({
+      menssage: 'Unauthorized'
+    });
+  }
+})
+
+app.get('/logout', function (req, res) {
+  console.log(sess);
+  req.session.destroy();
+  res.end('Sesion destruida.');
+});
+
+
 // Routes
-app.use(require('./routes/productos'));
+if(sess != "" && pass != ""){
+  app.use(require('./routes/productos'));
+}else{
+  res.status(401).send({
+    menssage: 'Unauthorized'
+  });
+}
 
 // Starting the server
 app.listen(app.get('port'), () => {
